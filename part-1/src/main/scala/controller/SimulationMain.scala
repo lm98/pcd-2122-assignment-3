34ed2571267eceec8a27dcf8ef19381e5ce66b3e
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-06-10 15:15:57 +0200

    rewritten model in scala

--- part-1/src/main/scala/controller/SimulationMain.scala
+++ part-1/src/main/scala/controller/SimulationMain.scala
@@ -3,7 +3,7 @@ package controller
 object SimulationMain:
   @main def startSim =
     new Simulator(
-      1000,
+      2,
       5000,
       Runtime.getRuntime.availableProcessors + 1
     ).execute()
