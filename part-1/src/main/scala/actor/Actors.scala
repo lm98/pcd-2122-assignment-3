0165bba15ba005195db6625319e1d0cff1d8c58e
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-06-10 10:56:59 +0200

    completed simulator actor behavior

--- part-1/src/main/scala/actor/Actors.scala
+++ part-1/src/main/scala/actor/Actors.scala
@@ -3,13 +3,14 @@ package actor
 
 import actor.BodyActor.computeTotalForceOnBody
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.util.Timeout
 import conc.model.{Body, Boundary, V2d}
+import jdk.javadoc.doclet.Reporter
 
 import concurrent.duration.DurationInt
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 
 case class Request(info: RequestBody, replyTo: ActorRef[Response])
 
@@ -22,19 +23,38 @@ case class Response(body: Body)
 
 import actor.RequestBody.*
 
-object MasterActor:
+object SimulatorActor:
   val dt = 0.001
+  val bounds = new Boundary(-4.00, -4.00, 4.00, 4.00)
 
   def apply(bodies: List[Body],vt: Double = 0.00, i: Int): Behavior[Response] =
     Behaviors setup { ctx =>
-      val slave = ctx.spawnAnonymous(SlaveActor())
+      val master = ctx.spawnAnonymous(MasterActor())
       given Timeout = 2.seconds
       given Scheduler = ctx.system.scheduler
       given ExecutionContext = ctx.executionContext
-      bodies.map(b => slave ? (replyTo => Request(UpdateVelocity(b, bodies, dt), replyTo)))
-      MasterActor(bodies, vt + dt, i + 1)
+      //Update velocities
+      val futureV = Future.sequence(bodies.map(b => master ? (replyTo => Request(UpdateVelocity(b, bodies, dt), replyTo))))
+      val updateV = Await.result(futureV, 2.seconds).map(r => r match
+        case Response(body) => body)
+      //Update positions
+      val futureP = Future.sequence(updateV.map(b => master ? (replyTo => Request(UpdatePosition(b, dt), replyTo))))
+      val updateP = Await.result(futureP, 2.seconds).map(r => r match
+        case Response(body) => body)
+      //Check Boundaries
+      val futureC = Future.sequence(updateP.map(b => master ? (replyTo => Request(CheckBoundary(b, bounds), replyTo))))
+      val updateC = Await.result(futureC, 2.seconds).map(r => r match
+        case Response(body) => body)
+      SimulatorActor(updateC, vt + dt, i + 1)
     }
 
+object MasterActor:
+  def apply(): Behavior[Request] =
+    Behaviors receive { (ctx, msg) =>
+      val slave = ctx.spawnAnonymous(SlaveActor())
+      slave ! msg
+      Behaviors.same
+    }
 
 object SlaveActor:
   def computeTotalForceOnBody(body: Body, bodies: List[Body]): V2d =
@@ -50,9 +70,9 @@ object SlaveActor:
       case UpdateVelocity(body: Body, others: List[Body], dt: Double) =>
         val totalForce = computeTotalForceOnBody(body, others)
         val acc = new V2d(totalForce).scalarMul(1.0/body.getMass)
-        body.updateVelocity(acc, dt) ; msg.replyTo ! Response(body) ; Behaviors.same
+        body.updateVelocity(acc, dt) ; msg.replyTo ! Response(body) ; Behaviors.stopped
       case UpdatePosition(body: Body, dt: Double) =>
-        body.updatePos(dt) ; msg.replyTo ! Response(body) ; Behaviors.same
+        body.updatePos(dt) ; msg.replyTo ! Response(body) ; Behaviors.stopped
       case CheckBoundary(body: Body, bounds: Boundary) =>
         body.checkAndSolveBoundaryCollision(bounds) ; msg.replyTo ! Response(body) ; Behaviors.stopped
     }
