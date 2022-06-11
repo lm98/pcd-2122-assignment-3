71810d8f5b0af420e87ddf638eba349bde792916
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-06-11 12:09:03 +0200

    add where should be init gui

--- part-1/src/main/scala/controller/SimulationMain.scala
+++ part-1/src/main/scala/controller/SimulationMain.scala
@@ -5,11 +5,4 @@ import view.ViewActor
 import controller.Simulator
 
 object SimulationMain:
-  @main def startSim =
-    val system: ActorSystem = ActorSystem.create("GUIsystem")
-//    var act: ActorRef = system.actorOf()
-    new Simulator(
-      2,
-      5000,
-      Runtime.getRuntime.availableProcessors + 1
-    ).execute()
+  @main def startSim = ??? //setup behavior, then spawn viewactor
--- part-1/src/main/scala/view/ViewActor.scala
+++ part-1/src/main/scala/view/ViewActor.scala
@@ -4,7 +4,9 @@ import akka.actor.AbstractActor
 import model.{Body, Boundary}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+
 import java.awt.{BorderLayout, Component, FlowLayout}
+import scala.language.postfixOps
 
 object ViewActor:
 
@@ -15,4 +17,10 @@ object ViewActor:
   export ViewCommands.*
 
   def apply(actorRef: ActorRef[_], bounds: Boundary, w: Int, h: Int): Behavior[ViewCommands] =
-    Behaviors.setup( ctx => ??? )
+    Behaviors.setup( ctx =>
+      val view = ViewUtils(ctx.self, bounds, w, h) //init gui
+      Behaviors.receiveMessage( msg =>
+        println(msg)
+        Behaviors.same
+      )
+    )
