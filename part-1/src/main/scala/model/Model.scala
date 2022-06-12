68065f231a5fe4f602ad2d591ef5dc7a6af896af
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-06-12 12:12:25 +0200

    refactor model

--- part-1/src/main/scala/model/Model.scala
+++ part-1/src/main/scala/model/Model.scala
@@ -3,13 +3,11 @@ package model
 import conc.model.exc.{InfiniteForceException, NullVectorException}
 import model.Objects2d.*
 import model.Objects2d.V2d.normalize
-
 import scala.annotation.{tailrec, targetName}
 
 case class Boundary(x0: Double, y0: Double, x1: Double, y1: Double)
 
 case class Body(id: Int, pos: P2d, vel: V2d, mass: Double)
-
 
 object Body:
   val REPULSIVE_CONST = 0.01
@@ -37,6 +35,14 @@ object Body:
   def getCurrentFrictionForce(body: Body): V2d =
     body.vel :* (- FRICTION_CONST)
 
+  def computeTotalForceOnBody(body: Body, bodies: List[Body]): V2d =
+    val totalForce: V2d = V2d(0,0)
+    bodies.filter(b => !b.equals(body)).foreach(b => {
+      val forceOtherBody = computeRepulsiveForceBy(body, b)
+      totalForce + forceOtherBody
+    })
+    totalForce + getCurrentFrictionForce(body)
+
   def checkAndSolveBoundaryCollision(body: Body, boundary: Boundary): Body =
     val x = body.pos.x ; val y = body.pos.y
     (x, y) match
@@ -48,7 +54,6 @@ object Body:
         Body(body.id, P2d(x, boundary.y1), V2d(body.vel.x, -body.vel.y), body.mass)
       case _ if y < boundary.y0 =>
         Body(body.id, P2d(x, boundary.y0), V2d(body.vel.x, -body.vel.y), body.mass)
-
 
 object Objects2d:
   case class V2d(x: Double, y: Double)
@@ -63,7 +68,6 @@ object Objects2d:
         if mod > 0 then
           V2d(v2d.x / mod, v2d.y / mod)
         else throw new NullVectorException
-
   extension (v: V2d)
     @targetName("scalarMul")
     def :*(k: Double): V2d = V2d(v.x * k, v.y * k)
