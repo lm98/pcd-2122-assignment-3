91834fed27b6dce55be0097f74d1db07b99ceb4b
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-06-12 12:13:20 +0200

    refactored actors to use new model implementation

--- part-1/src/main/scala/actor/Actors.scala
+++ part-1/src/main/scala/actor/Actors.scala
@@ -4,8 +4,10 @@ import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.util.Timeout
-import conc.model.{Body, Boundary, V2d}
 import jdk.javadoc.doclet.Reporter
+import model.Objects2d.V2d
+import model.{Body, Boundary}
+
 import concurrent.duration.DurationInt
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 
@@ -22,7 +24,7 @@ import actor.RequestInfo.*
 
 object SimulatorActor:
   val dt = 0.001
-  val bounds = new Boundary(-4.00, -4.00, 4.00, 4.00)
+  val bounds = Boundary(-4.00, -4.00, 4.00, 4.00)
 
   def apply(bodies: List[Body],vt: Double = 0.00, i: Int): Behavior[Response] =
     Behaviors setup { ctx =>
@@ -40,22 +42,16 @@ object MasterActor:
     }
 
 object SlaveActor:
-  def computeTotalForceOnBody(body: Body, bodies: List[Body]): V2d =
-    val totalForce: V2d = new V2d(0,0)
-    bodies.filter(b => !b.equals(body)).foreach(b => {
-      val forceOtherBody = body.computeRepulsiveForceBy(b)
-      totalForce.sum(forceOtherBody)
-    })
-    totalForce.sum(body.getCurrentFrictionForce)
+  import model.Body.*
 
   def apply(): Behavior[Request] =
     Behaviors receiveMessage { msg => msg.info match
       case UpdateVelocity(body: Body, others: List[Body], dt: Double) =>
         val totalForce = computeTotalForceOnBody(body, others)
-        val acc = new V2d(totalForce).scalarMul(1.0/body.getMass)
-        body.updateVelocity(acc, dt) ; msg.replyTo ! Response(body) ; Behaviors.stopped
+        val acc = totalForce :* (1.0/body.mass)
+         msg.replyTo ! Response(updateVelocity(body, acc, dt)) ; Behaviors.stopped
       case UpdatePosition(body: Body, dt: Double) =>
-        body.updatePos(dt) ; msg.replyTo ! Response(body) ; Behaviors.stopped
+        msg.replyTo ! Response(updatePos(body, dt)) ; Behaviors.stopped
       case CheckBoundary(body: Body, bounds: Boundary) =>
-        body.checkAndSolveBoundaryCollision(bounds) ; msg.replyTo ! Response(body) ; Behaviors.stopped
+        msg.replyTo ! Response(checkAndSolveBoundaryCollision(body, bounds)) ; Behaviors.stopped
     }
