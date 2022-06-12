6ccece368bc01cf2279743ae49d531442b8f9bf1
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-06-12 19:41:57 +0200

    implemented simulator actor, master and slave communication

--- part-1/src/main/scala/actor/Actors.scala
+++ part-1/src/main/scala/actor/Actors.scala
@@ -1,5 +1,6 @@
 package actor
 
+import actor.MasterActor.Commands
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.scaladsl.AskPattern.Askable
@@ -9,49 +10,68 @@ import model.Objects2d.V2d
 import model.{Body, Boundary}
 
 import concurrent.duration.DurationInt
-import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise, blocking}
+import scala.util.Success
 
-case class Request(info: RequestInfo, replyTo: ActorRef[Response])
 
-enum RequestInfo:
+enum Task:
   case UpdateVelocity(body: Body, others: List[Body], dt: Double)
   case UpdatePosition(body: Body, dt: Double)
   case CheckBoundary(body: Body, boundary: Boundary)
 
-case class Response(body: Body)
-
-import actor.RequestInfo.*
+import Task.*
 
 object SimulatorActor:
-  val dt = 0.001
-  val bounds = Boundary(-4.00, -4.00, 4.00, 4.00)
+  sealed trait Commands
+  case class Start() extends Commands
+  case class Update(info: Task, bodies: List[Body]) extends Commands
 
-  def apply(bodies: List[Body],vt: Double = 0.00, i: Int): Behavior[Response] =
-    Behaviors setup { ctx =>
-      val master = ctx.spawnAnonymous(MasterActor())
-      bodies.foreach(b => master ! Request(UpdateVelocity(b, bodies, dt), ctx.self))
-      SimulatorActor(bodies, vt + dt, i + 1)
+  val dt = 0.001
+
+  def apply(bodies: List[Body],vt: Double = 0.00, i: Int): Behavior[Commands] =
+    Behaviors receive { (ctx, msg) => msg match
+      case Start() =>
+        val master = ctx.spawnAnonymous(MasterActor(List(), bodies.size, ctx.self))
+        bodies.foreach(b => master ! MasterActor.Request(UpdateVelocity(b, bodies, dt)))
+        Behaviors.same
+      case Update(UpdateVelocity(_,_,_), updatedBodies) =>
+        val master = ctx.spawnAnonymous(MasterActor(List(), bodies.size, ctx.self))
+        updatedBodies.foreach(b => master ! MasterActor.Request(UpdatePosition(b, dt)))
+        SimulatorActor(updatedBodies, vt, i)
+      case Update(UpdatePosition(_,_), updatedBodies) =>
+        val master = ctx.spawnAnonymous(MasterActor(List(), bodies.size, ctx.self))
+        val bounds = Boundary(-4.00, -4.00, 4.00, 4.00)
+        updatedBodies.foreach(b => master ! MasterActor.Request(CheckBoundary(b, bounds)))
+        SimulatorActor(updatedBodies, vt, i)
+      case Update(CheckBoundary(_,_), updatedBodies) => SimulatorActor(updatedBodies, vt + dt, i + 1)
     }
 
 object MasterActor:
-  def apply(): Behavior[Request] =
-    Behaviors receive { (ctx, msg) =>
-      val slave = ctx.spawnAnonymous(SlaveActor())
-      slave ! msg
-      Behaviors.same
+  sealed trait Commands
+  case class Request(info: Task) extends Commands
+  case class Update(info: Task, result: Body) extends Commands
+
+  def apply(bodies: List[Body], nBodies: Int, replyTo: ActorRef[SimulatorActor.Commands]): Behavior[Commands] =
+    Behaviors receive { (ctx, msg) => msg match
+      case Request(info) =>
+        ctx.spawnAnonymous(SlaveActor()) ! SlaveActor.Request(info, ctx.self) ; Behaviors.same
+      case Update(info, result) => bodies.size match
+        case _ if bodies.size < nBodies => MasterActor(bodies :+ result, nBodies, replyTo)
+        case _ if bodies.size == nBodies - 1 => replyTo ! SimulatorActor.Update(info, bodies) ; Behaviors.stopped
     }
 
 object SlaveActor:
   import model.Body.*
+  case class Request(info: Task, replyTo: ActorRef[MasterActor.Commands])
 
   def apply(): Behavior[Request] =
     Behaviors receiveMessage { msg => msg.info match
       case UpdateVelocity(body: Body, others: List[Body], dt: Double) =>
         val totalForce = computeTotalForceOnBody(body, others)
         val acc = totalForce :* (1.0/body.mass)
-         msg.replyTo ! Response(updateVelocity(body, acc, dt)) ; Behaviors.stopped
+         msg.replyTo ! MasterActor.Update(msg.info, updateVelocity(body, acc, dt)) ; Behaviors.stopped
       case UpdatePosition(body: Body, dt: Double) =>
-        msg.replyTo ! Response(updatePos(body, dt)) ; Behaviors.stopped
+        msg.replyTo ! MasterActor.Update(msg.info, updatePos(body, dt)) ; Behaviors.stopped
       case CheckBoundary(body: Body, bounds: Boundary) =>
-        msg.replyTo ! Response(checkAndSolveBoundaryCollision(body, bounds)) ; Behaviors.stopped
+        msg.replyTo ! MasterActor.Update(msg.info, checkAndSolveBoundaryCollision(body, bounds)) ; Behaviors.stopped
     }
