f5b93abea9882837d7139fb8adf81fb2b5d85ca7
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-07-01 16:51:16 +0200

    add behaviors setup in viewactor

--- part-2/src/main/scala/actors/ViewActor.scala
+++ part-2/src/main/scala/actors/ViewActor.scala
@@ -1,13 +1,30 @@
 package actors
 
 import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import model.Zone
 import view.ViewImpl
 
-enum ViewActorCommands:
-  case FixZone(zoneId: Int)
 
-//todo wrong, must be one zone each actor
-class ViewActor(var zones: List[Zone], ctx: ActorContext[_], val zoneId: Int):
-  var view: ViewImpl = new ViewImpl(zones)
+
+
+object ViewActor:
+  enum ViewActorCommands:
+    case FixZone(zoneId: Int)
+  val viewService: ServiceKey[ViewActorCommands] = ServiceKey[ViewActorCommands]("ViewService")
+
+  def apply(zones: List[Zone], zoneId: Int): Behavior[ViewActorCommands] = //todo wrong, might be one zone each actor
+    var view: ViewImpl = new ViewImpl(zones)
+    Behaviors setup {ctx =>
+      ctx.system.receptionist ! Receptionist.register(viewService, ctx.self)
+      running(ctx)
+    }
+
+  private def running(ctx: ActorContext[ViewActorCommands]): Behavior[ViewActorCommands] =
+
+    Behaviors receiveMessage { msg => msg match
+      case ViewActorCommands.FixZone(zoneId) => ???
+
+    }
+
