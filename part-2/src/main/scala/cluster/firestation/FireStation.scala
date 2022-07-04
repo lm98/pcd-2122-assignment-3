ac373c5a37ebcbc82d7e645faceb536fb3953af0
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-07-04 15:21:37 +0200

    made firestation notify viewActor

--- part-2/src/main/scala/cluster/firestation/FireStation.scala
+++ part-2/src/main/scala/cluster/firestation/FireStation.scala
@@ -1,5 +1,6 @@
 package cluster.firestation
 
+import actors.ViewActor
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
@@ -12,6 +13,7 @@ import concurrent.duration.*
 object FireStation:
   sealed trait Event
   private case class RainGaugesUpdated(newSet: Set[ActorRef[RainGauge.Event]]) extends Event
+  private case class ViewActorsUpdated(newSet: Set[ActorRef[ViewActor.Event]]) extends Event
   case class NotifyAlarmOn() extends Event with CborSerializable
   case class NotifyAlarmOff() extends Event with CborSerializable
   val FireStationServiceKey: ServiceKey[FireStation.Event] = ServiceKey[FireStation.Event]("FireStation")
@@ -21,26 +23,43 @@ object FireStation:
       val subscriptionAdapter = ctx.messageAdapter[Receptionist.Listing] {
         case RainGauge.ListenerServiceKey.Listing(newSet) =>
           RainGaugesUpdated(newSet)
+        case ViewActor.viewActorServiceKey.Listing(newSet) =>
+          ViewActorsUpdated(newSet)
       }
       ctx.system.receptionist ! Receptionist.Subscribe(RainGauge.ListenerServiceKey, subscriptionAdapter)
+      ctx.system.receptionist ! Receptionist.Subscribe(ViewActor.viewActorServiceKey, subscriptionAdapter)
       ctx.system.receptionist ! Receptionist.Register(FireStationServiceKey, ctx.self)
-      running(ctx, IndexedSeq.empty, 0)
+
+      running(ctx, IndexedSeq.empty, IndexedSeq.empty,0)
     }
 
   private def running(ctx: ActorContext[FireStation.Event],
                       rainGauges: IndexedSeq[ActorRef[RainGauge.Event]],
+                      viewActors: IndexedSeq[ActorRef[ViewActor.Event]],
                       alarmNotifications: Int): Behavior[FireStation.Event] =
     Behaviors receiveMessage { msg => msg match
       case RainGaugesUpdated(newSet) =>
         ctx.log.info(s"There are now ${rainGauges.size} rain gauges")
-        running(ctx,newSet.toIndexedSeq, alarmNotifications)
+        running(ctx,newSet.toIndexedSeq, viewActors, alarmNotifications)
+      case ViewActorsUpdated(newSet) =>
+        running(ctx, rainGauges, newSet.toIndexedSeq, alarmNotifications)
       case NotifyAlarmOn() =>
         val notifications = alarmNotifications + 1
         ctx.log.info(s"Received $notifications notifications")
         if notifications >= rainGauges.size then
           ctx.log.info("Taking care of the alarm")
-          running(ctx, rainGauges, 0)
+          //viewActors foreach { _ ! ViewActor.AlarmOn() } @todo modify ViewActor.Alarm()
+          alarmed(ctx, rainGauges, viewActors)
         else
-          running(ctx, rainGauges, notifications)
-      case NotifyAlarmOff() => Behaviors.same
+          running(ctx, rainGauges, viewActors, notifications)
+      case _ => Behaviors.same
+    }
+
+  private def alarmed(ctx: ActorContext[FireStation.Event],
+                      rainGauges: IndexedSeq[ActorRef[RainGauge.Event]],
+                      viewActors: IndexedSeq[ActorRef[ViewActor.Event]]): Behavior[FireStation.Event] =
+    Behaviors receiveMessage { msg => msg match
+      case NotifyAlarmOff() =>
+        ctx.log.info("Alarm managed")
+        running(ctx, rainGauges, viewActors, 0)
     }
