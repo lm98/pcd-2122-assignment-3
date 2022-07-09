0ac7a3dc3c7ed940b0fb9e4a0c77d4927f980231
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-07-09 12:53:40 +0200

    add firestation and point2d case class

--- part-2/src/main/scala/model/Model.scala
+++ part-2/src/main/scala/model/Model.scala
@@ -1,7 +1,5 @@
 package model
 
-import java.awt.geom.Point2D
-
 object Costants:
   val defaultHeight: Int = 150
   val defalutWidth: Int = 200
@@ -11,10 +9,16 @@ class Model:
     case Ok
     case Alarm
     case Managing
+  enum FireStationState:
+    case Free
+    case Occupied
   case class RainGauge(zoneID: Int, pos: Point2D)
   case class Zone(id: Int, zoneState: ZoneState, bounds: RectangleBounds):
     var rainGauges: List[RainGauge] = List.empty
     def changeState(newState: ZoneState): Unit = zoneState = newState
     def addRainGauge(rainGauge: RainGauge): Unit = rainGauges = rainGauges :+ rainGauge
   case class RectangleBounds(topLeft: Point2D, height: Int = Costants.defaultHeight, width: Int = Costants.defalutWidth):
-    def getBottomRight: Point2D = Point2D(topLeft.getX + width, topLeft.getY + height)
+    def getBottomRight: Point2D = Point2D(topLeft.x + width, topLeft.y + height)
+  case class Point2D(x: Int, y: Int)
+  case class FireStation(zoneID: Int, fireStationState: FireStationState, pos: Point2D)
+    def changeState(newState: FireStationState): Unit = fireStationState = newState
