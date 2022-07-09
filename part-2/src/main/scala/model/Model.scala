e853f53a791c4e1cc7ec91a6c7b39d3050d79bc1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-07-09 12:57:50 +0200

    refactor

--- part-2/src/main/scala/model/Model.scala
+++ part-2/src/main/scala/model/Model.scala
@@ -4,22 +4,21 @@ object Costants:
   val defaultHeight: Int = 150
   val defalutWidth: Int = 200
 
-class Model:
-  enum ZoneState:
-    case Ok
-    case Alarm
-    case Managing
-  enum FireStationState:
-    case Free
-    case Occupied
-  case class RainGauge(zoneID: Int, pos: Point2D)
-  case class Zone(id: Int, var zoneState: ZoneState, var fireStation: FireStation, bounds: RectangleBounds):
-    var rainGauges: List[RainGauge] = List.empty
-    def changeState(newState: ZoneState): Unit = zoneState = newState
-    def addRainGauge(rainGauge: RainGauge): Unit = rainGauges = rainGauges :+ rainGauge
-    def setFireStation(fs: FireStation): Unit = fireStation = fs
-  case class RectangleBounds(topLeft: Point2D, height: Int = Costants.defaultHeight, width: Int = Costants.defalutWidth):
-    def getBottomRight: Point2D = Point2D(topLeft.x + width, topLeft.y + height)
-  case class Point2D(x: Int, y: Int)
-  case class FireStation(zoneID: Int, var fireStationState: FireStationState, pos: Point2D)
-    def changeState(newState: FireStationState): Unit = fireStationState = newState
+enum ZoneState:
+  case Ok
+  case Alarm
+  case Managing
+enum FireStationState:
+  case Free
+  case Occupied
+case class RainGauge(zoneID: Int, pos: Point2D)
+case class Zone(id: Int, var zoneState: ZoneState, var fireStation: FireStation, bounds: RectangleBounds):
+  var rainGauges: List[RainGauge] = List.empty
+  def changeState(newState: ZoneState): Unit = zoneState = newState
+  def addRainGauge(rainGauge: RainGauge): Unit = rainGauges = rainGauges :+ rainGauge
+  def setFireStation(fs: FireStation): Unit = fireStation = fs
+case class RectangleBounds(topLeft: Point2D, height: Int = Costants.defaultHeight, width: Int = Costants.defalutWidth):
+  def getBottomRight: Point2D = Point2D(topLeft.x + width, topLeft.y + height)
+case class Point2D(x: Int, y: Int)
+case class FireStation(zoneID: Int, var fireStationState: FireStationState, pos: Point2D)
+  def changeState(newState: FireStationState): Unit = fireStationState = newState
